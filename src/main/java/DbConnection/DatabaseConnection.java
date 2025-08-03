913cdbd27446d2f5090c43cfbc0b70305027e3d0
reniers.J <renie@DESKTOP-E5F043U>
2025-08-03 19:00:43 +0200

    Update voor verbinding DatabaseConnection werkt nu

---
+++ src/main/java/DbConnection/DatabaseConnection.java
@@ -0,0 +1,22 @@
+package DbConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    public static Connection connect() {
+        String connectionUrl =
+                "jdbc:sqlserver://localhost:1433;databaseName=AirLineDb;user=airlineUser;password=airline;encrypt=false;";
+
+        try {
+            Connection conn = DriverManager.getConnection(connectionUrl);
+            System.out.println("✅ Verbonden met database!");
+            return conn;
+        } catch (SQLException e) {
+            System.err.println("❌ Fout bij verbinding: " + e.getMessage());
+            return null;
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import DbConnection.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        DatabaseConnection.connect();
+
     }
 }
